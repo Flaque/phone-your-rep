5161278e8231c6e123c2f913133377aae6b85f05
M. Simon Borg <msimonborg@gmail.com>
2016-12-06 08:00:34 -0500

    switch endpoint to dev server

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
-// var ENDPOINT = 'http://localhost:3000/reps'
-var ENDPOINT = 'https://phone-your-rep-api.herokuapp.com/reps'
+var ENDPOINT = 'http://localhost:3000/reps'
+// var ENDPOINT = 'https://phone-your-rep-api.herokuapp.com/reps'
 
 
 /**
